8494a2bb2066d6e7a1b052c809895418ba69f166
anshukavashishtha <163641700+anshukavashishtha@users.noreply.github.com>
2025-03-02 07:37:32 +0530

    Add files via upload

---
+++ Intership.py
@@ -0,0 +1,38 @@
+import cv2
+import os
+import string
+
+img = cv2.imread("/Users/anshukavashishtha/Desktop/photo.jpeg") 
+
+msg = input("Enter secret message: ")
+password = input("Enter a passcode: ")
+
+height, width, _ = img.shape  
+
+if len(msg) > height * width:
+    print("Message too long for the image!")
+    exit()
+
+index = 0
+for i in range(height):
+    for j in range(width):
+        if index < len(msg):
+            img[i, j, 0] = ord(msg[index]) 
+            index += 1
+
+cv2.imwrite("encryptedImage.jpg", img)
+os.system("open encryptedImage.jpg") 
+
+message = ""
+index = 0
+pas = input("Enter passcode for Decryption: ")
+
+if password == pas:
+    for i in range(height):
+        for j in range(width):
+            if index < len(msg):
+                message += chr(img[i, j, 0]) 
+                index += 1
+    print("Decryption message:", message)
+else:
+    print("YOU ARE NOT AUTHORIZED")
